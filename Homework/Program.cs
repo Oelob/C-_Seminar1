b69e4716e5e2d33dfa746a3e36dd506034824dd9
Ivan <ab09@inbox.ru>
2022-10-14 01:07:38 +0500

    Added first task

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2. На вход программа принимает два числа и выдает большее и меньшее
+Console.Clear();
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 == number2)
+{
+    Console.WriteLine("Числа равны");
+}
+else
+    if (number1 > number2)
+    {
+        Console.WriteLine("Максимальное число:" + number1 + ", минимальное число: " + number2);
+    }
+    else
+    {
+        Console.WriteLine("Максимальное число:" + number2 + ", минимальное число: " + number1);
+    }
