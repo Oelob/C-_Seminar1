bca2b0cee04b49629541f127dafaab501ad1bcfb
Ivan <ab09@inbox.ru>
2022-10-14 11:59:24 +0500

    Task4_Begining

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -1,20 +1,25 @@
 ﻿// Задача 2. На вход программа принимает два числа и выдает большее и меньшее
-Console.Clear();
-Console.Write("Введите первое число: ");
-int number1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число: ");
-int number2 = Convert.ToInt32(Console.ReadLine());
+// Console.Clear();
+// Console.Write("Введите первое число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
 
-if (number1 == number2)
-{
-    Console.WriteLine("Числа равны");
-}
-else
-    if (number1 > number2)
-    {
-        Console.WriteLine("Максимальное число:" + number1 + ", минимальное число: " + number2);
-    }
-    else
-    {
-        Console.WriteLine("Максимальное число:" + number2 + ", минимальное число: " + number1);
-    }
+// if (number1 == number2)
+// {
+//     Console.WriteLine("Числа равны");
+// }
+// else
+//     if (number1 > number2)
+//     {
+//         Console.WriteLine("Максимальное число:" + number1 + ", минимальное число: " + number2);
+//     }
+//     else
+//     {
+//         Console.WriteLine("Максимальное число:" + number2 + ", минимальное число: " + number1);
+//     }
+
+// // Задача 4. Программа принимает на вход три числа и выдает максимальное 
+
+
+
