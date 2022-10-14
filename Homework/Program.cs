0c44e2eff3b741decc546c8288e9b0afceb30ec3
Ivan <ab09@inbox.ru>
2022-10-14 12:30:48 +0500

    Task6_Completed

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -21,23 +21,39 @@ Console.Clear();
 
 // // Задача 4. Программа принимает на вход три числа и выдает максимальное 
 
-Console.Write("Введите первое число: ");
-int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите первое число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите второе число: ");
-int number2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите третье число: ");
-int number3 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите третье число: ");
+// int number3 = Convert.ToInt32(Console.ReadLine());
 
-int max = number1;
+// int max = number1;
 
-if (number2 > max)
+// if (number2 > max)
+// {
+//     max = number2;
+// }
+//     if (number3 > max)
+//     {
+//         max = number3;
+//     }
+// Console.Write("Максимальное число: " + max);
+
+
+// // Задача 6. Проверка четности числа
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number % 2;
+
+if (result == 0)
 {
-    max = number2;
+    Console.Write("Число " + number + " четное");
 }
-    if (number3 > max)
-    {
-        max = number3;
-    }
-Console.Write("Максимальное число: " + max);
+else
+{
+    Console.Write("Число " + number + " нечетное");
+}    
