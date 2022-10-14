8e9df6a4c0d8fcf258ee4d7e1593fbbcd6427657
Ivan <ab09@inbox.ru>
2022-10-14 12:14:50 +0500

    Task4_complited

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Задача 2. На вход программа принимает два числа и выдает большее и меньшее
-// Console.Clear();
+Console.Clear();
 // Console.Write("Введите первое число: ");
 // int number1 = Convert.ToInt32(Console.ReadLine());
 // Console.Write("Введите второе число: ");
@@ -21,5 +21,23 @@
 
 // // Задача 4. Программа принимает на вход три числа и выдает максимальное 
 
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
 
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
 
+Console.Write("Введите третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+
+if (number2 > max)
+{
+    max = number2;
+}
+    if (number3 > max)
+    {
+        max = number3;
+    }
+Console.Write("Максимальное число: " + max);
