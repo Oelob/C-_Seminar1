f734b48fedcf8a089d330434579a423aeece894d
Ivan <ab09@inbox.ru>
2022-10-14 15:02:17 +0500

    Task8_Completed

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -1,74 +1,85 @@
-﻿// Задача 2. На вход программа принимает два числа и выдает большее и меньшее
-Console.Clear();
-// Console.Write("Введите первое число: ");
-// int number1 = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Введите второе число: ");
-// int number2 = Convert.ToInt32(Console.ReadLine());
-
-// if (number1 == number2)
-// {
-//     Console.WriteLine("Числа равны");
-// }
-// else
-//     if (number1 > number2)
-//     {
-//         Console.WriteLine("Максимальное число:" + number1 + ", минимальное число: " + number2);
-//     }
-//     else
-//     {
-//         Console.WriteLine("Максимальное число:" + number2 + ", минимальное число: " + number1);
-//     }
-
-// // Задача 4. Программа принимает на вход три числа и выдает максимальное 
-
-// Console.Write("Введите первое число: ");
-// int number1 = Convert.ToInt32(Console.ReadLine());
-
-// Console.Write("Введите второе число: ");
-// int number2 = Convert.ToInt32(Console.ReadLine());
-
-// Console.Write("Введите третье число: ");
-// int number3 = Convert.ToInt32(Console.ReadLine());
-
-// int max = number1;
-
-// if (number2 > max)
-// {
-//     max = number2;
-// }
-//     if (number3 > max)
-//     {
-//         max = number3;
-//     }
-// Console.Write("Максимальное число: " + max);
-
-
-// // Задача 6. Проверка четности числа
-
-// Console.Write("Введите число: ");
-// int number = Convert.ToInt32(Console.ReadLine());
-// int result = number % 2;
-
-// if (result == 0)
-// {
-//     Console.Write("Число " + number + " четное");
-// }
-// else
-// {
-//     Console.Write("Число " + number + " нечетное");
-// }    
-
-// // Задача 8. 
-
-Console.Write("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-int count = 1;
-int diff = count % 2;
-
-while (count < num)
-    
-    if (diff == 0)
+﻿internal class Program
+{
+    private static void Main(string[] args)
     {
-        Console.Write(count + ",");
+        // Задача 2. На вход программа принимает два числа и выдает большее и меньшее
+        Console.Clear();
+        // Console.Write("Введите первое число: ");
+        // int number1 = Convert.ToInt32(Console.ReadLine());
+        // Console.Write("Введите второе число: ");
+        // int number2 = Convert.ToInt32(Console.ReadLine());
+
+        // if (number1 == number2)
+        // {
+        //     Console.WriteLine("Числа равны");
+        // }
+        // else
+        //     if (number1 > number2)
+        //     {
+        //         Console.WriteLine("Максимальное число:" + number1 + ", минимальное число: " + number2);
+        //     }
+        //     else
+        //     {
+        //         Console.WriteLine("Максимальное число:" + number2 + ", минимальное число: " + number1);
+        //     }
+
+        // // Задача 4. Программа принимает на вход три числа и выдает максимальное 
+
+        // Console.Write("Введите первое число: ");
+        // int number1 = Convert.ToInt32(Console.ReadLine());
+
+        // Console.Write("Введите второе число: ");
+        // int number2 = Convert.ToInt32(Console.ReadLine());
+
+        // Console.Write("Введите третье число: ");
+        // int number3 = Convert.ToInt32(Console.ReadLine());
+
+        // int max = number1;
+
+        // if (number2 > max)
+        // {
+        //     max = number2;
+        // }
+        //     if (number3 > max)
+        //     {
+        //         max = number3;
+        //     }
+        // Console.Write("Максимальное число: " + max);
+
+
+        // // Задача 6. Проверка четности числа
+
+        // Console.Write("Введите число: ");
+        // int number = Convert.ToInt32(Console.ReadLine());
+        // int result = number % 2;
+
+        // if (result == 0)
+        // {
+        //     Console.Write("Число " + number + " четное");
+        // }
+        // else
+        // {
+        //     Console.Write("Число " + number + " нечетное");
+        // }    
+
+        // // Задача 8. 
+
+        Console.Write("Введите число: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        int count = 1;
+        
+        
+        while (count <= num)
+        {
+            int diff = count % 2;
+            if (diff == 0)
+            {
+                Console.Write(count + " ");
+            }
+            
+            count++;
+        }
+
+          
     }
-    count +=1;
+}
